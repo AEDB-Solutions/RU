59ef7e604e6fb4dd85316aa728697317b1ab6d58
garciajoaopaulo <garciajoaounb@gmail.com>
2017-06-26 21:33:06 -0300

    Add files via upload
    Funcão saldo melhorada

--- DbController.php
+++ DbController.php
@@ -233,4 +233,17 @@ Class Controller {
 
       }
 
+    function saldo($matricula){
+    $myConnect = new ConnectDB();
+    $myConnect->Connect();
+    $conn = $myConnect->conn;
+    $sql = "SELECT sum(Valor) FROM Compras WHERE Matricula = '$matricula'";
+    $rs = mysqli_query($conn, $sql);
+      if(FALSE == $rs) die("Select sum failed: ".mysqli_error);
+    $row = mysqli_fetch_row($rs);
+    $sum= $row[0];
+    //print "Seu saldo total é R$: $sum";
+    //echo json_encode($sum);
+    echo $sum;
+  }
 }
--- saldo.php
+++ saldo.php
@@ -1,19 +1,11 @@
 <?PHP 
 
+include ('DbController.php');
 
-	$conexao = mysqli_connect("localhost","root","", "Test");
-
-
-
-		$sql = "SELECT sum(saldo) AS total FROM saldo";
-    	$rs = mysqli_query($conexao, 'SELECT sum(saldo) AS total FROM saldo');
-      if(FALSE == $rs) die("Select sum failed: ".mysqli_error);
-      $row = mysqli_fetch_row($rs);
-      $sum= $row[0];
-      print "Seu saldo total: R$: $sum";
-   
-
-   		mysqli_close($conexao);
+$meuobjeto = json_decode(file_get_contents('php://input'));
+$extrato = new Controller();
+$extrato->saldo($meuobjeto->matricula);
+?>
 	
 ?>
 
